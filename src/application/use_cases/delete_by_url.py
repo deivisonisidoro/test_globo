74f6f6ad02076028ad128369036dba37cba21502
Deivison isidoro <deivisonisidoro@gmail.com>
2024-09-28 13:14:54 -0300

    feat: creating use case to delete a video by url

---
+++ src/application/use_cases/delete_by_url.py
@@ -0,0 +1,45 @@
+from src.domain.dtos.video_response import VideoResponseDTO
+from src.domain.exceptions.validation_error.video_not_found_error import (
+    VideoNotFoundError,
+)
+from src.domain.repositories.videos import AbstractVideoRepository
+from src.domain.enums.error_messages import ErrorMessagesEnum
+from src.domain.use_cases.delete_by_url import AbstractDeleteVideoByUrlUseCase
+
+
+class DeleteVideoByUrlUseCase(AbstractDeleteVideoByUrlUseCase):
+    """Concrete use case for deleting a video by URL."""
+
+    def __init__(self, video_repository: AbstractVideoRepository):
+        """Initializes the DeleteVideoByUrlUseCase.
+
+        Args:
+            video_repository (AbstractVideoRepository): The repository to delete video entities.
+        """
+        self.video_repository = video_repository
+
+    def execute(self, url: str) -> VideoResponseDTO:
+        """Deletes a video by its URL.
+
+        Args:
+            url (str): The URL of the video to delete.
+
+        Returns:
+            VideoResponseDTO: The DTO containing the status code and message.
+
+        Raises:
+            VideoNotFoundError: If no video with the given URL exists.
+        """
+        video = self.video_repository.find_by_url(url)
+
+        if not video:
+            raise VideoNotFoundError(
+                message=ErrorMessagesEnum.NO_VIDEOS_FOUND.value,
+                name="VideoNotFoundError",
+            )
+
+        self.video_repository.delete(video)
+
+        return VideoResponseDTO(
+            status_code=204, data={"message": "Video deleted successfully."}
+        )
---
+++ src/domain/use_cases/delete_by_url.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+from src.domain.dtos.video_response import VideoResponseDTO
+
+
+class AbstractDeleteVideoByUrlUseCase(ABC):
+    """Abstract base class for deleting a video by URL."""
+
+    @abstractmethod
+    def execute(self, url: str) -> VideoResponseDTO:
+        """Executes the use case to delete a video by its URL.
+
+        Args:
+            url (str): The URL of the video to delete.
+
+        Returns:
+            VideoResponseDTO: The DTO containing the status code and message.
+
+        Raises:
+            Exception: If an error occurs during deletion.
+        """
+        pass

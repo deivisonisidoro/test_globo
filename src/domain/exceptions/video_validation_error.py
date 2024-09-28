ae4314e4b676f99a09ec1e81026be65c8c277b54
Deivison isidoro <deivisonisidoro@gmail.com>
2024-09-27 22:28:08 -0300

    feat: adding new custom error

--- src/domain/exceptions/video_validation_error.py
+++ src/domain/exceptions/video_validation_error.py
@@ -50,3 +50,15 @@ class InvalidUrlError(VideoValidationError):
     """
 
     pass
+
+
+class DuplicateUrlError(VideoValidationError):
+    """Exception raised for duplicate video URLs.
+
+    This exception is used to indicate that a provided URL already exists
+    in the system, and thus cannot be used to create a new video.
+    It is a subclass of VideoValidationError, specifically for duplicate
+    URL-related issues.
+    """
+
+    pass

9b86def2dbf14928a0a03f4d14e64a381fde0a9e
Deivison isidoro <deivisonisidoro@gmail.com>
2024-09-27 22:27:50 -0300

    feat: adding new message

--- src/domain/enums/error_messages.py
+++ src/domain/enums/error_messages.py
@@ -8,6 +8,9 @@ class ErrorMessagesEnum(Enum):
     Attributes:
         INVALID_URL (str): Error message when the URL is invalid.
             The URL must start with 'http' or 'https'.
+        DUPLICATE_URL (str): Error message when a video with the same URL
+            already exists in the system.
     """
 
     INVALID_URL = "URL inválida. A URL deve começar com 'http' ou 'https'."
+    DUPLICATE_URL = "A video with the same URL already exists."

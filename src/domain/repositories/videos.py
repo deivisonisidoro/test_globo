68f0d7672b3b58b941f8c1e1a3d394665c6dff81
Deivison isidoro <deivisonisidoro@gmail.com>
2024-09-27 22:28:30 -0300

    feat: creating AbstractVideoRepository

---
+++ src/domain/repositories/videos.py
@@ -0,0 +1,35 @@
+from abc import ABC, abstractmethod
+from typing import Optional
+
+from src.domain.entities.video import VideoEntity
+
+
+class AbstractVideoRepository(ABC):
+    """Abstract base class for video repositories.
+
+    This class defines the methods that any concrete video repository must implement.
+    """
+
+    @abstractmethod
+    def create(self, video: VideoEntity) -> VideoEntity:
+        """Creates a new video in the repository.
+
+        Args:
+            video (VideoEntity): The video entity to create.
+
+        Returns:
+            VideoEntity: The created video entity.
+        """
+        pass
+
+    @abstractmethod
+    def find_by_url(self, url: str) -> Optional[VideoEntity]:
+        """Finds a video by URL.
+
+        Args:
+            url (str): The URL of the video to find.
+
+        Returns:
+            Optional[VideoEntity]: The found video entity or None if not found.
+        """
+        pass

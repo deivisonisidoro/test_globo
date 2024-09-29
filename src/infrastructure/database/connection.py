8d65381ef4fb19c6b5fd6d83ea82f92dc679c017
Deivison isidoro <deivisonisidoro@gmail.com>
2024-09-29 09:26:45 -0300

    docs: adding docstring

--- src/infrastructure/database/connection.py
+++ src/infrastructure/database/connection.py
@@ -14,6 +14,16 @@ Base = declarative_base()
 
 
 def get_db():
+    """
+    Dependency that provides a database session for each request.
+
+    This function manages the lifecycle of a database session. It opens a
+    new session and ensures that the session is closed after the request
+    is completed.
+
+    Yields:
+        SessionLocal: The database session object.
+    """
     db = SessionLocal()
     try:
         yield db

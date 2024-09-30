a30f122e4ff567719edd045c214541a4cd7cffb6
Deivison isidoro <deivisonisidoro@gmail.com>
2024-09-30 19:47:12 -0300

    feat: adding new fetching to list the videos

--- src/services/api/videos.ts
+++ src/services/api/videos.ts
@@ -29,3 +29,28 @@ export const addVideo = async (url: string) => {
     }
   };
   
+  export const listVideos = async () => {
+    if (!apiUrl) {
+      throw new Error("API URL is not defined");
+    }
+  
+    try {
+      const response = await fetch(`${apiUrl}/videos`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+  
+      const data = await response.json();
+  
+      if (!response.ok) {
+        throw new Error(data.detail);
+      }
+  
+      return data; // Presume que a resposta seja uma lista de vídeos
+    } catch (error) {
+      console.error("Error fetching videos:", error);
+      throw error;
+    }
+  };
